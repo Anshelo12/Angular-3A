dfbf081f8fcad3058c53329b844375c4e029ad9a
Dylan Cruz <aal.proano@yavirac.edu.ec>
2023-06-08 07:33:57 -0500

    product.component

--- pv-angular-2023-1/src/app/pages/product/product.component.ts
+++ pv-angular-2023-1/src/app/pages/product/product.component.ts
@@ -10,4 +10,14 @@ export class ProductComponent {
   constructor(private httpClient:HttpClient){
     
   }
+  ngOnInit(): void{
+    this.getProducts();
+  }
+  getProducts(){
+    const response = this.httpClient.get('http://api.escuela.js.co/api/vi/products').subscribe(
+    response=>{
+    console.log(response);
+    }
+    );
+  };
 }
